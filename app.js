e456d79f1d0a47ca264cf94c94457ac5da193dab
Sathishkumar Subramanian <neosathishgct@gmail.com>
2018-09-10 17:46:04 +0530

    comments on global object and modularity

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 var member = "";
 
 console.log(global.member);
+
+//avoid writing local variables in the global scope
+//go for modularity - by a concept called 'module'
+
+//every node module should have one module as the main module
+//app.js is the main module here
+
+console.log(module);
