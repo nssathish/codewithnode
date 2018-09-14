159cf2aa04206198b390914f2c4556c01fdb31f6
Sathishkumar Subramanian <neosathishgct@gmail.com>
2018-09-14 12:07:58 +0530

    learning about the os module briefly

--- app.js
+++ app.js
@@ -12,3 +12,9 @@ logger.log("sathish");
 // learing the node's path module
 const pathObj = require("./learning node modules/path_module");
 logger.log(pathObj);
+
+//learning the node's OS module
+const osObj = require("./learning node modules/os_module");
+logger.log(osObj);
+logger.log("Total memory: " + osObj.osTotalMem);
+logger.log("Free Memory: " + osObj.osFreeMem);
---
+++ learning node modules/os_module.js
@@ -0,0 +1,10 @@
+const os = require("os");
+
+var totalMem = os.totalmem();
+var freeMem = os.freemem();
+
+console.log("Total memory: " + totalMem);
+console.log("Free memory: " + freeMem);
+
+exports.osTotalMem = totalMem;
+exports.osFreeMem = freeMem;
