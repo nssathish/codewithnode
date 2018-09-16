38bf099e6d16811d7951528842f1db256a868644
Sathishkumar Subramanian <neosathishgct@gmail.com>
2018-09-16 05:56:49 +0530

    Extending EventEmitter for the logger module

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const logger = require("./logger");
+const Logger = require("./logger");
 // console.log(logger);
 
 // logger = 1;
@@ -6,33 +6,38 @@ const logger = require("./logger");
 
 // if a fucntion is exported directly via 'require'
 // we can call the function directly
-logger.eventName = "messageLogged";
+const logger = new Logger();
+logger.on("logging", eventArgs => {
+  console.log("lisntener called", eventArgs);
+});
 logger.log("sathish");
-console.log(logger);
+// // logger.eventName = "messageLogged";
+// logger.log("sathish");
+// console.log(logger);
 
-//learing the node's path module
-console.log("1.Learning path module");
-const pathObj = require("./learning node modules/path_module");
-logger.log(pathObj);
-console.log("-------");
+// //learing the node's path module
+// console.log("1.Learning path module");
+// const pathObj = require("./learning node modules/path_module");
+// logger.log(pathObj);
+// console.log("-------");
 
-//learning the node's OS module
-console.log("2.Learning os module");
-const osObj = require("./learning node modules/os_module");
-logger.log(osObj);
-console.log("Total memory: " + osObj.osTotalMem);
-console.log("Free Memory: " + osObj.osFreeMem);
-console.log("-------");
+// //learning the node's OS module
+// console.log("2.Learning os module");
+// const osObj = require("./learning node modules/os_module");
+// logger.log(osObj);
+// console.log("Total memory: " + osObj.osTotalMem);
+// console.log("Free Memory: " + osObj.osFreeMem);
+// console.log("-------");
 
-//learning the node's filesystem module
-console.log("3.Learning filesystem module");
-const files = require("./learning node modules/fs_module");
-console.log("-------");
+// //learning the node's filesystem module
+// console.log("3.Learning filesystem module");
+// const files = require("./learning node modules/fs_module");
+// console.log("-------");
 
-//loearning the nodes events module
-console.log("4.Learning events module");
-const events = require("./learning node modules/event_module");
+// //loearning the nodes events module
+// console.log("4.Learning events module");
+// const events = require("./learning node modules/event_module");
 
-//creating a logging event module to get familiar with the syntax
-console.log("5.Creating a logging event");
-const loggingEvent = require("./learning node modules/logging_event");
+// //creating a logging event module to get familiar with the syntax
+// console.log("5.Creating a logging event");
+// const loggingEvent = require("./learning node modules/logging_event");
--- logger.js
+++ logger.js
@@ -3,26 +3,37 @@
 // console.log(__dirname);
 // console.log(module);
 const EventEmitter = require("events");
-const events = new EventEmitter();
-var eventName;
-var url = "http://mylogger.io./log";
+// const events = new EventEmitter();
+// var eventName;
+var url = "http://mylogger.io/log";
 
-events.on(eventName, eventArg => {
-  console.log(eventArg);
-});
-function log(message) {
-  // send an HTTP request
-  // console.log(message);
-  console.log(eventName);
-  events.emit(eventName, { id: 1, url: url, message: message });
-}
+// events.on(eventName, eventArg => {
+//   console.log(eventArg);
+// });
+// function log(message) {
+//   // send an HTTP request
+//   // console.log(message);
+//   console.log(eventName);
+//   events.emit(eventName, { id: 1, url: url, message: message });
+// }
 
 // instead of creating an object namely exports.log
 // if the module has only one function we can export the function directly
 // like belwo
 //module.exports = log;
-exports.log = log;
-exports.eventName = eventName;
+
+class Logger extends EventEmitter {
+  log(message) {
+    // send an HTTP request
+    // console.log(message);
+    console.log("logging");
+    this.emit("logging", { id: 1, url: url, message: message });
+  }
+}
+
+module.exports = Logger;
+// exports.log = log;
+// exports.eventName = eventName;
 // module.exports.log = log;
 // module.exports.endpoint = url; //information detail
 // });
