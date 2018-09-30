c41858383e7aa75be3faa81c5aa074bcbd4341be
Sathishkumar Subramanian <neosathishgct@gmail.com>
2018-09-30 13:09:16 +0530

    GET/POST method

--- express-work/index.js
+++ express-work/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+var courses = [
+  { id: 1, name: "Course1" },
+  { id: 2, name: "Course2" },
+  { id: 3, name: "Course3" },
+  { id: 4, name: "Course4" }
+];
+
 app.get("/", (req, res) => {
   res.send("Hello world");
   res.end();
@@ -18,7 +27,26 @@ app.get("/api/courses/:year/:month", (req, res) => {
     JSON.stringify({ "field params": req.params, "query params": req.query })
   );
 });
-console.log(process.env.PORT);
+
+//GET course by ID
+
+app.get("/api/courses/:id", (req, res) => {
+  const result = courses.find(c => c.id === parseInt(req.params.id));
+  if (!result) res.status(404).send("The course for the given ID is not found");
+  res.send(result);
+});
+
+app.post("/api/courses", (req, res) => {
+  const result = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+
+  courses.push(result);
+  res.send(result);
+});
+
+console.log("Environmental port: " + process.env.PORT);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
