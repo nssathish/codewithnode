47263ab010b2799f403ea6d092f0d40ba515f432
Sathishkumar Subramanian <neosathishgct@gmail.com>
2018-10-19 11:33:39 +0530

    validation rules and POST request body

--- express-work/genres.js
+++ express-work/genres.js
@@ -1,18 +1,22 @@
 const express = require("express")
 const app = express()
 
+const joi = require("joi") //for validation rules
+app.use(express.json()) //to let node know about the request body
+
 //genres
-const genres = [
+var genres = [
     {"id": 1, "name": "Comedy"},
     {"id": 2, "name": "Rom-com"},
-    {"id": 1, "name": "sit-com"},
-    {"id": 1, "name": "spoof"}
+    {"id": 3, "name": "sit-com"},
+    {"id": 4, "name": "spoof"}
 ]
 app.get('/',(req,res) => {
     res.send(JSON.stringify(genres))
 })
 
 app.post('/api/genres', (req,res) => {
+    console.log(req.body)
     genres.push(
         {
             id : genres.length + 1,
@@ -21,5 +25,12 @@ app.post('/api/genres', (req,res) => {
     )
     res.send(JSON.stringify(genres))
 })
-const port = process.env.port || '3000'
+
+const validateGenres = (genre) => {
+    const schema = {
+        name: joi.string().min(3).required()
+    }
+    return joi.validate(genre,schema)
+}
+const port = process.env.PORT || '3000'
 app.listen(port, () => console.log(`Listening to port ${port}`))
