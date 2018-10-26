c72eeabdc484768c25b795bf11a5f7076a180bbf
Sathish Kumar <neosathishgct@gmail.com>
2018-10-26 05:57:49 +0530

    How to set an env based code and making the env variables dynamic by using config npm package

--- express-work/index.js
+++ express-work/index.js
@@ -1,27 +1,48 @@
 const Joi = require("joi");
 const express = require("express");
+const config = require("config");
 
 //express middleware functions
-const morgan = require('morgan')
-const helmet = require('helmet')
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const app = express();
 
 //custom middleware
-const logger = require('./logger')
-const auth = require('./auth')
+const logger = require("./logger");
+const auth = require("./auth");
 
 //installing built-in middleware
 app.use(express.json());
-app.use(express.urlencoded( { extended: true})) //it allow POST information as url encoded form data like 'key=value&key=value&key=value' in the url
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })); //it allows POST information as url encoded form data like 'key=value&key=value&key=value' in the url
+app.use(express.static("public"));
 
-app.use(morgan('tiny'))
-app.use(helmet())
+//anything can be chosen here to check the environment
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`env: ${app.get("env")}`);
+
+//any middleware or routing can be used based on the environment we have chosen
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan enabled...");
+}
+//For the enterprise applications we need to configure the environment dynamically based on the config files
+//For that we are going to use the npm package 'config'
+//P.S: the configuration file names should '.json' of NODE_ENV name, the 'config' package works that way (case insensitive)
+//and all the configuration files should be under ./config folder
+console.log(`Name of the App: ${config.get("name")}`);
+console.log(`Host: ${config.get("mail.host")}`);
+
+//Now to store the password information that are required for the application we need to
+//create a file called 'custom-environment-variables.json' as per the 'config' package
+//this will map the environment variables to the ones used in the json file
+console.log(`password: ${config.get("credentials.password")}`);
+
+app.use(helmet());
 
 //installing custom middleware
-app.use(logger)
-app.use(auth)
+app.use(logger);
+app.use(auth);
 
 var courses = [
   { id: 1, name: "Course1" },
