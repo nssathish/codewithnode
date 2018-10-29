39c234b7d4b13f66959c00db50db3b0176d532f5
Sathish Kumar <neosathishgct@gmail.com>
2018-10-29 07:48:40 +0530

    Naming functions as callback by passing it has reference

--- express-work/asyncdemo.js
+++ express-work/asyncdemo.js
@@ -1,26 +1,47 @@
-console.log("Before");
-getUser(1, user => {
-  console.log("User: ", user);
-  const username = user.gitHubUsername;
-  getRepositories(username, repositories => {
-    console.log("Repositories: ", repositories);
-  });
-});
+const debug = require("debug")("app:async");
+
+if (debug.enabled === false) {
+  debug.enabled = true;
+  console.log("Debugger enabled...");
+}
+
+debug("Before");
+getUser(1, getRepositories);
+debug("After");
+
+function getCommits(repo) {
+  debug("Repos", repo);
+  getCommits(repo, displayCommits);
+}
 
 function getUser(id, callback) {
   setTimeout(() => {
-    console.log("Reading user from the database...");
+    debug("Reading user from the database...");
     callback({ id: id, gitHubUsername: "sathish" });
   }, 2000);
 }
+
 function getRepositories(username, callback) {
   setTimeout(() => {
-    console.log("Getting repositories from github");
-    var repos = { username: "", repositories: [] };
-    repos.username = username;
-    repos.repositories = ["repo1", "repo2", "repo3"];
+    debug("Getting repositories from github");
+    const repos = ["repo1", "repo2", "repo3"];
+    console.log(callback);
     callback(repos);
   }, 2000);
 }
+function getRepositories(user) {
+  //since everything is async here when gitRepositories is called 'user' object will be undefined
+  if (user !== undefined) {
+    debug("User", user);
+    getRepositories(user.gitHubUsername, getCommits);
+  }
+}
 
-console.log("After");
+function getCommits(repo, callback) {
+  setTimeout(() => {
+    callback(["commit1", "commit2", "commit3"]);
+  }, 2000);
+}
+function displayCommits(commits) {
+  debug(commits);
+}
